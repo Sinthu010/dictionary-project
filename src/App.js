454ae4ef639aedf720ec76b878562a43231ea473
Sinthu010 <sinthuja010@gmail.com>
2023-06-14 18:43:36 +0100

    initial commit
    open the terminal and run the command npx create-react-app
    run npm start
    insert images using react and header

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./coding-cartoon.jpeg";
+import logo2 from "./coding-cartoon2.jpeg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo2} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
