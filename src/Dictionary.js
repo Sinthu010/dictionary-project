c02d423a6191d098e6f9c4ffc92942cdba69a486
Sinthu010 <sinthuja010@gmail.com>
2023-06-16 15:26:36 +0100

    integrate API
    Uses SheCodes Dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   // state is called keyword
   // to change setKeyword component called
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     //prevent page resetting
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+    let apiKey = "2abt2f6340do47141b6f92d3a14301be";
+    let apiURL = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiURL).then(handleResponse);
   }
 
   function updateKeyword(event) {
